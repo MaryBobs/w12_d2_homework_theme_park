116fac51a26261fe275bdcf380cb9b3a423670b0
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2020-01-14 18:29:43 +0000

    implements ISEcurity in Rollercoaster and tests

--- ThemeParkStartPoint/src/test/java/attractions/RollercoasterTest.java
+++ ThemeParkStartPoint/src/test/java/attractions/RollercoasterTest.java
@@ -2,16 +2,23 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class RollercoasterTest {
 
     RollerCoaster rollerCoaster;
+    Visitor visitor1;
+    Visitor visitor2;
+    Visitor visitor3;
 
     @Before
     public void setUp() {
         rollerCoaster = new RollerCoaster("Blue Ridge", 10);
+        visitor1 = new Visitor(18, 2.2, 30.00);
+        visitor2 = new Visitor(10, 2.2, 30.00);
+        visitor3 = new Visitor(18, 1.2, 30.00);
     }
 
     @Test
@@ -27,5 +34,16 @@ public class RollercoasterTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, rollerCoaster.getVisitCount());
+    }
+
+    @Test
+    public void isAllowedToRideRollercoaster() {
+        assertEquals(true, rollerCoaster.isAllowedTo(visitor1));
+    }
+
+    @Test
+    public void isNotAllowedToRideRollerCoaster() {
+        assertEquals(false, rollerCoaster.isAllowedTo(visitor2));
+        assertEquals(false, rollerCoaster.isAllowedTo(visitor3));
     }
 }
