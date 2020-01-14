c27911b594cda69f5940093c8b822bf55165cfc9
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2020-01-14 18:57:55 +0000

    implements ITickets in Dodgems and tests, MVP completed

--- ThemeParkStartPoint/src/test/java/attractions/DodgemTest.java
+++ ThemeParkStartPoint/src/test/java/attractions/DodgemTest.java
@@ -2,16 +2,21 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class DodgemTest {
 
     Dodgems dodgems;
+    Visitor visitor1;
+    Visitor visitor2;
 
     @Before
     public void setUp() throws Exception {
         dodgems = new Dodgems("Bumper Cars", 5);
+        visitor1 = new Visitor(10, 2.2, 30.00);
+        visitor2 = new Visitor(18, 2.2, 30.00);
     }
 
 
@@ -28,5 +33,16 @@ public class DodgemTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, dodgems.getVisitCount());
+    }
+
+    @Test
+    public void hasDefaultPrice() {
+        assertEquals(4.50, dodgems.defaultPrice(), 0.01);
+    }
+
+    @Test
+    public void chargesHalfPriceIfUnder12() {
+        assertEquals(2.25, dodgems.priceForVisitor(visitor1), 0.01);
+        assertEquals(4.50, dodgems.priceForVisitor(visitor2), 0.01);
     }
 }
